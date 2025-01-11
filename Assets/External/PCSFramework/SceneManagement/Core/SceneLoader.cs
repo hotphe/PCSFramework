4a9a511e5a82c3aa30c0f9fc56922b479d11ded6
hotphe <hotpheno@gmail.com>
2025-01-12 05:50:24 +0900

    테스트 코드 삭제
    테스트 코드 삭제

--- Assets/External/PCSFramework/SceneManagement/Core/SceneLoader.cs
+++ Assets/External/PCSFramework/SceneManagement/Core/SceneLoader.cs
@@ -8,13 +8,5 @@ namespace PCS.SceneManagement
         {
             //SceneManager.Instance.LoadSceneAsync<LobbyPresenter>(scene => scene.InitializeAsync(2)).Forget();
         }
-        public static void ToMyScene2()
-        {
-            SceneManager.Instance.LoadSceneAsync<MyScene2Presenter>(scene => scene.InitializeAsync(2)).Forget();
-        }
-        public static void ToMyScene1()
-        {
-            SceneManager.Instance.LoadSceneAsync<MyScene1Presenter>(scene => scene.InitializeAsync(2)).Forget();
-        }
     }
 }

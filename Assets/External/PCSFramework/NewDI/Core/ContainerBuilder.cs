32d6ae4cbe23d8e86a72cf3346f4f729515ae575
hotphe <hotpheno@gmail.com>
2025-01-24 19:39:21 +0900

    Rebind 여부 기능 추가

--- Assets/External/PCSFramework/NewDI/Core/ContainerBuilder.cs
+++ Assets/External/PCSFramework/NewDI/Core/ContainerBuilder.cs
@@ -5,6 +5,7 @@ using PCS.DI.Bind;
 using PCS.DI.Generic;
 using PCS.DI.Extension;
 using System.Linq;
+using UnityEngine;
 
 
 namespace PCS.DI.Core
@@ -16,9 +17,10 @@ namespace PCS.DI.Core
         
         public readonly List<Binding> Bindings = new List<Binding>();
 
+        public bool AllowRebinding { get; private set; } = false;
+
         //Bind with type
         public Binding AddSingleton(Type concrete) => AddSingleton(concrete, concrete);
-        public Binding AddSingleton<T>(params Type[] contracts) => AddSingleton(typeof(T),contracts);
         public Binding AddSingleton(Type concrete, params Type[] contracts)
         {
             var binding = Binding.Validated(new SingletonTypeResolver(concrete), concrete, contracts);
@@ -27,7 +29,6 @@ namespace PCS.DI.Core
         }
 
         public Binding AddScoped(Type concrete) => AddScoped(concrete, concrete);
-        public Binding AddScoped<T>(params Type[] contracts) => AddScoped(typeof(T), contracts);
         public Binding AddScoped(Type concrete, params Type[] contracts)
         {
             var binding = Binding.Validated(new ScopedTypeResolver(concrete), concrete, contracts);
@@ -36,7 +37,6 @@ namespace PCS.DI.Core
         }
 
         public Binding AddTransient(Type concrete) => AddTransient(concrete, concrete);
-        public Binding AddTransient<T>(params Type[] contracts) => AddTransient(typeof(T), contracts);
         public Binding AddTransient(Type concrete, params Type[] contracts)
         {
             var binding = Binding.Validated(new TransientTypeResolver(concrete), concrete, contracts);
@@ -66,6 +66,12 @@ namespace PCS.DI.Core
             return this;
         }
 
+        public ContainerBuilder AllowRebind(bool allowRebinding)
+        {
+            AllowRebinding = allowRebinding;
+            return this;
+        }
+
         public Container Build()
         {
             var disposables = new DisposableCollection();
@@ -92,24 +98,68 @@ namespace PCS.DI.Core
             }
 
             // Owned resolvers
-            foreach (var binding in Bindings)
+            if (AllowRebinding)
             {
-                disposables.Add(binding.Resolver);
-                if (binding.Name == null)
+                foreach (var binding in Bindings)
                 {
-                    foreach (var contract in binding.Contracts)
-                        resolversByContract.GetOrAdd(contract, _ => new List<IObjectResolver>()).Add(binding.Resolver);
-                }else
-                {
-                    foreach (var contract in binding.Contracts)
+                    disposables.Add(binding.Resolver);
+                    if (binding.Name == null)
                     {
-                        if(resolversByName.TryGetValue(contract,out var dict))
+                        foreach (var contract in binding.Contracts)
+                            resolversByContract.GetOrAdd(contract, _ => new List<IObjectResolver>()).Add(binding.Resolver);
+                    }
+                    else
+                    {
+                        foreach (var contract in binding.Contracts)
                         {
-                            dict.GetOrAdd(binding.Name, _ => new List<IObjectResolver>()).Add(binding.Resolver);
-                        }else
+                            if (resolversByName.TryGetValue(contract, out var dict))
+                            {
+                                dict.GetOrAdd(binding.Name, _ => new List<IObjectResolver>()).Add(binding.Resolver);
+                            }
+                            else
+                            {
+                                resolversByName.Add(contract, new Dictionary<string, List<IObjectResolver>>());
+                                resolversByName[contract].GetOrAdd(binding.Name, _ => new List<IObjectResolver>()).Add(binding.Resolver);
+                            }
+                        }
+                    }
+                }
+            }else
+            {
+                foreach (var binding in Bindings)
+                {
+                    disposables.Add(binding.Resolver);
+                    if (binding.Name == null)
+                    {
+                        foreach (var contract in binding.Contracts)
                         {
-                            resolversByName.Add(contract, new Dictionary<string, List<IObjectResolver>>());
-                            resolversByName[contract].GetOrAdd(binding.Name, _ => new List<IObjectResolver>()).Add(binding.Resolver);
+                            if (resolversByContract.ContainsKey(contract))
+                            {
+                                Debug.Log($"{contract} type is already rosolved.");
+                                continue;
+                            }
+                            resolversByContract.Add(contract, new List<IObjectResolver>());
+                            resolversByContract[contract].Add(binding.Resolver);
+                        }
+                    }
+                    else
+                    {
+                        foreach (var contract in binding.Contracts)
+                        {
+                            if (resolversByName.TryGetValue(contract, out var dict))
+                            {
+                                if(dict.ContainsKey(binding.Name))
+                                {
+                                    Debug.Log($"{contract} type named {binding.Name} is already rosolved.");
+                                    continue;
+                                }
+                                dict.GetOrAdd(binding.Name, _ => new List<IObjectResolver>()).Add(binding.Resolver);
+                            }
+                            else
+                            {
+                                resolversByName.Add(contract, new Dictionary<string, List<IObjectResolver>>());
+                                resolversByName[contract].GetOrAdd(binding.Name, _ => new List<IObjectResolver>()).Add(binding.Resolver);
+                            }
                         }
                     }
                 }
@@ -118,11 +168,6 @@ namespace PCS.DI.Core
             var container = new Container(Name, Parent, resolversByContract,resolversByName, disposables);
             
             return container;
-        }
-
-        public void Dispose()
-        {
-            
         }
     }
 }
